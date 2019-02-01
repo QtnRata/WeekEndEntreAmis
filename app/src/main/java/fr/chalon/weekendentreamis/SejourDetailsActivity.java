db85c59794a2164f26ca33aab7c40331722f6534
Dylan Gatefossey <dgatefossey@gmail.com>
2019-02-01 18:24:58 +0100

    Ajout des fichiers de ressources pour la traduction + remplacé tous les textes

--- app/src/main/java/fr/chalon/weekendentreamis/SejourDetailsActivity.java
+++ app/src/main/java/fr/chalon/weekendentreamis/SejourDetailsActivity.java
@@ -2,6 +2,7 @@ package fr.chalon.weekendentreamis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SejourDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class SejourDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_sejour);
+
     }
 }
